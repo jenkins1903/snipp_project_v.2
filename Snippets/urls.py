d2caeb4eaa468d5f02c143ea2f710d34614e00a5
jenkins1903 <evgeniya_raldugina@vk.com>
2024-02-29 16:13:33 +0300

    Initial commit

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
+
+
 
 urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page),
     path('snippets/list', views.snippets_page),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
